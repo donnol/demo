7ad4476aa05e650e57c27c5297067db625e9c1c6
liujiandong <liujiandong@iprexmed.com>
2017-11-21 17:15:34 +0800

    vue-static -- mod

--- vue-static/src/main.js
+++ vue-static/src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// import HelloWorld from './components/HelloWorld'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -10,6 +11,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  template: '<app/>',
+  components: {
+    'App': App
+  }
 })

ca248d95bbc979402f252d08d25336eb1ea9897d
liujiandong <liujiandong@iprexmed.com>
2017-11-30 10:46:28 +0800

    const -- add

---
+++ const/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+var b = 1
+
+// const a = func() int {
+// 	return 1
+// }()
+
+// const a = b
+const a = 1
+
+func main() {
+	fmt.Println(a)
+
+	// a = 2
+	// fmt.Println(a)
+}

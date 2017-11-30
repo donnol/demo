eef2d2b282bb10340f69c2493b370589e6da6404
liujiandong <liujiandong@iprexmed.com>
2017-11-30 10:25:46 +0800

    vue-static -- add vue-resource

--- vue-static/src/components/List.vue
+++ vue-static/src/components/List.vue
@@ -13,6 +13,20 @@
 export default {
   name: 'List',
   data() {
+    // GET
+    // url 必须是相对路径，如：'helloworld' 不能有 '/'，否则无法使用 this.$http.options.root
+    this.$http.get('helloworld').then(
+      response => {
+        // success callback
+        console.log('hello successed.')
+        console.log(response)
+      },
+      response => {
+        // error callback
+        console.log('hello failed.')
+        console.log(response)
+      }
+    )
     return {
       msg: '以往寒风躲闪行，今秋尽是路人多。',
       content: `分布式系统之微服务架构设计要点
@@ -26,6 +40,10 @@ export default {
       author: 'jd'
     }
   }
+  // 覆盖全局变量 this.$http.options.root
+  // http: {
+  //   root: 'https://www.google.com'
+  // }
 }
 </script>
 
--- vue-static/src/main.js
+++ vue-static/src/main.js
@@ -3,6 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+Vue.http.options.root = 'http://127.0.0.1:8080/#' // 后面没有 '/'
+Vue.http.headers.common['Authorization'] = 'Basic YXBpOnBhc3N3b3Jk'
+// Vue.http.options.emulateJSON = true // 服务器不支持 application/json 时使用
+Vue.http.options.emulateHTTP = true // 服务器不支持 PUT PATCH DELETE 等方法
 
 Vue.config.productionTip = false
 

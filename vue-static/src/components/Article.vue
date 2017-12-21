3f280181f5d571405bbabf477dc083d313e35b2d
liujiandong <liujiandong@iprexmed.com>
2017-12-21 16:57:48 +0800

    vue-static -- thought

--- vue-static/src/components/Article.vue
+++ vue-static/src/components/Article.vue
@@ -8,7 +8,7 @@
 export default {
   name: 'article',
   data() {
-    let content = `事务的隔离级别：读未提交；读已提交；可重复读；串行；`
+    let content = `事务的隔离级别：读未提交；读已提交；可重复读；串行；自我逃避，自我惩罚；实例-文档-源码`
     return {
       article: content
     }

3885f9f72b1db2f1ffe0b1ef6eec10f505ed4e18
liujiandong <liujiandong@iprexmed.com>
2018-02-08 16:55:08 +0800

    golang -- add open_same_file

---
+++ golang/open_same_file/process2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	file := "about.md"
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE, os.ModeAppend)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	for i := 0; i < 1000; i++ {
+		_, err = f.WriteString("2")
+		if err != nil {
+			panic(err)
+		}
+	}
+}

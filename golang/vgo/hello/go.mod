799fdcc507b688324f809c9d508a1333d3393ae4
liujiandong <liujiandong@iprexmed.com>
2018-02-28 17:06:59 +0800

    golang -- add vgo

---
+++ golang/vgo/hello/go.mod
@@ -0,0 +1,6 @@
+module "github.com/donnol/hello"
+
+require (
+	"golang.org/x/text" v0.0.0-20171214130843-f21a4dfb5e38
+	"rsc.io/quote" v1.5.2
+)
---
+++ golang/vgo/hello/hello.go
@@ -0,0 +1,11 @@
+package main // import "github.com/donnol/hello"
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Hello())
+}

5a235da0d5b0177718b715bdacd59ff41ae18377
liujiandong <liujiandong@iprexmed.com>
2018-03-30 17:05:58 +0800

    golang -- stdlib image update

--- golang/stdlib/image/main.go
+++ golang/stdlib/image/main.go
@@ -24,6 +24,8 @@ func (m myImage) At(x, y int) (cc color.Color) {
 }
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	var _ image.Image = myImage{}
 
 	image.RegisterFormat("my image", "magic", func(io.Reader) (image.Image, error) {
@@ -43,6 +45,34 @@ func main() {
 		log.Fatal(err)
 	}
 	err = png.Encode(w, alpha)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p := color.Palette{}
+	palet := image.NewPaletted(r, p)
+	for i := 0; i < 50; i++ {
+		palet.Set(i, i, cr)
+	}
+	w, err = os.OpenFile("paletted.png", os.O_CREATE|os.O_TRUNC|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = png.Encode(w, palet)
+	if err != nil {
+		// log.Fatal(err) // png: invalid format: bad palette length: 0
+	}
+
+	point := image.Pt(50, 50)
+	log.Printf("%v\n", point)
+
+	uniform := image.NewUniform(cr)
+	log.Printf("%+v\n", uniform)
+	w, err = os.OpenFile("uniform.png", os.O_CREATE|os.O_TRUNC|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = png.Encode(w, uniform)
 	if err != nil {
 		log.Fatal(err)
 	}

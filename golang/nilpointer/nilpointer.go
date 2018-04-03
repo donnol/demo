e8b66c5afb8672ecaee45409c681df2b7cdf9593
liujiandong <liujiandong@iprexmed.com>
2018-04-03 17:27:09 +0800

    golang -- nilpointer update

--- golang/nilpointer/nilpointer.go
+++ golang/nilpointer/nilpointer.go
@@ -4,6 +4,35 @@ import "fmt"
 
 var sptr *string
 
+type PM struct {
+	// pm *pm
+	*pm
+}
+
+type pm struct {
+	Title *string
+}
+
 func main() {
-	fmt.Println(*sptr)
+	// 	panic: runtime error: invalid memory address or nil pointer dereference
+	// [signal SIGSEGV: segmentation violation code=0x1 addr=0x0 pc=0x4831b0]
+	// 1
+	// fmt.Println(*sptr)
+	// 2
+	// instance := PM{}
+	// fmt.Println(instance.Title)
+	// fmt.Println(instance.pm.Title)
+
+	instance := PM{pm: &pm{}}
+	fmt.Println(instance.Title)
+	fmt.Println(instance.pm.Title)
+
+	var a, b = "a", "b"
+	_ = a
+	_ = b
+
+	instance.Title = &a
+	fmt.Println(instance.Title, *instance.Title)
+	instance.pm.Title = &b
+	fmt.Println(instance.pm.Title, *instance.pm.Title)
 }
